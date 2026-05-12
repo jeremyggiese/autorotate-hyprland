237427cc8d7fc4e0fe3a1e969dae1e4f5783fb38
Jeremy Giese <jeremyggiese@gmail.com>
2026-05-12 16:09:15 -0400

    Update rotate-screen.sh for V0.55
    Updated to use eval so that autorotate works on latest version. Note that device name must be set for touch device manually.

--- rotate-screen.sh
+++ rotate-screen.sh
@@ -12,8 +12,11 @@ TOGGLE_FILE="$HOME/.config/hypr/rotation-toggle"
 [ -f "$TOGGLE_FILE" ] || echo "1" > "$TOGGLE_FILE"
 
 monitor-sensor | while read -r line; do
+	toggle_file_cont=$(<$TOGGLE_FILE)
+  if [[ $toggle_file_cont -eq "1" ]]; then
+	echo "enabled"
   if [[ $line == *"orientation changed: normal"* ]]; then
-    TRANSFORM=0
+   TRANSFORM=0
   elif [[ $line == *"orientation changed: right-up"* ]]; then
     TRANSFORM=3
   elif [[ $line == *"orientation changed: left-up"* ]]; then
@@ -24,5 +27,14 @@ monitor-sensor | while read -r line; do
     continue
   fi
 
-  hyprctl keyword monitor "$MONITOR,$MODE,$POSITION,$SCALE,transform,$TRANSFORM"
+  hyprctl eval 'hl.monitor ({ output="'$MONITOR'",mode= "'$MODE'",position="'$POSITION'",transform= '$TRANSFORM'})'
+  hyprctl eval 'hl.device ({name="elan901c:00-04f3:2a41",  transform=' $TRANSFORM'})'
+ #echo $MONITOR
+ #echo $MODE
+ #echo $POSITION
+ #echo $TRANSFORM
+else
+	echo "disabled"
+  fi
+
 done
